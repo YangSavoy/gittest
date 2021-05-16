ab2e67ec778fd020dfe8d8a0f9db1ba264db4b01
Savoy Yang <ja2468jimmy@gmail.com>
2021-05-17 01:10:07 +0800

    add cc

---
+++ HelloEclipse.java
@@ -0,0 +1,13 @@
+package xxx;
+
+public class HelloEclipse {
+	
+	private String aa;
+	private String bb;
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		//註解快速鍵：ctrl+/		
+	}
+
+}
